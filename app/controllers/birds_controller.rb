33eeae676c7ceda7fb64e14c7f1272ce85333e52
kevinkkimutai <kiruikevinkim@gmail.com>
2023-03-14 15:51:13 +0300

    Assignment completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,23 @@ class BirdsController < ApplicationController
     end
   end
 
+  # update
+  def update
+
+    bird = Bird.find_by(id: params[:id])
+    if Bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "bird not found" }, status: :not_found
+    end
+
+    end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
